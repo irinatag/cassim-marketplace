d17bd905d5e0e489c4d23b52ef39cab5a7f225c0
tophat8855 <hrfarley@gmail.com>
2015-03-11 09:54:54 -0700

    feature testing for editing a product passes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+
     if @product.save
       flash[:success] = "Product saved"
       redirect_to product_path(@product.id)
@@ -19,6 +20,22 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+
+    if @product.save
+      flash[:success] = "Product saved"
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
   end
 
   private
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -48,27 +48,27 @@ feature 'CRUDing products' do
     expect(page).to have_content("Amazon Link")
   end
 
-  scenario 'user views a product' do
+  scenario 'view a product' do
     Product.create!(
-    name: 'Product 2',
-    description: 'This is another product',
-    quantity: 2000,
-    price: 50.00,
-    moq: 50,
-    when_ready: 'now',
-    brand: 'ACME',
-    sku: 'qwerty',
-    mfr: 'asdf',
-    msrp: 74.99,
-    manufacturer: 'ACME Co.',
-    category1: 'household goods',
-    category2: 'kitchen appliances',
-    upc: 123456789,
-    restrictions: 'none',
-    warehouse_zip: '60013',
-    warranty: '2 year limited manufacturer warranty',
-    link1: "www.google.com",
-    link1_title: "Google",
+      name: 'Product 2',
+      description: 'This is another product',
+      quantity: 2000,
+      price: 50.00,
+      moq: 50,
+      when_ready: 'now',
+      brand: 'ACME',
+      sku: 'qwerty',
+      mfr: 'asdf',
+      msrp: 74.99,
+      manufacturer: 'ACME Co.',
+      category1: 'household goods',
+      category2: 'kitchen appliances',
+      upc: 123456789,
+      restrictions: 'none',
+      warehouse_zip: '60013',
+      warranty: '2 year limited manufacturer warranty',
+      link1: "www.google.com",
+      link1_title: "Google",
     )
 
     visit products_path
@@ -92,5 +92,76 @@ feature 'CRUDing products' do
     expect(page).to have_content('2 year limited manufacturer warranty')
     expect(page).to have_content('www.google.com')
     expect(page).to have_content('Google')
+  end
+
+  scenario 'edit a product' do
+    Product.create!(
+      name: 'Product 2',
+      description: 'This is another product',
+      quantity: 2000,
+      price: 50.00,
+      moq: 50,
+      when_ready: 'now',
+      brand: 'ACME',
+      sku: 'qwerty',
+      mfr: 'asdf',
+      msrp: 74.99,
+      manufacturer: 'ACME Co.',
+      category1: 'household goods',
+      category2: 'kitchen appliances',
+      upc: 123456789,
+      restrictions: 'none',
+      warehouse_zip: '60013',
+      warranty: '2 year limited manufacturer warranty',
+      link1: "www.google.com",
+      link1_title: "Google",
+    )
+
+    visit products_path
+    click_on 'Product 2'
+    click_on 'Edit'
+    fill_in "Name", with: "Product 1"
+    fill_in "Description", with: "Description of product 1"
+    fill_in "Quantity", with: "1000"
+    fill_in "Price", with: "22.50"
+    fill_in "Moq", with: "50"
+    fill_in "When ready", with: "now"
+    fill_in "Brand", with: "ACME"
+    fill_in "Sku", with: "fkrr2qBZ"
+    fill_in "Mfr", with: "6kqX6FbX"
+    fill_in "Msrp", with: "69.99"
+    fill_in "Manufacturer", with: "ACME Co."
+    fill_in "Category1", with: "electronics"
+    fill_in "Category2", with: "kitchen appliances"
+    fill_in "Upc", with: "702037443"
+    fill_in "Restrictions", with: "none"
+    fill_in "Warehouse zip", with: "02345"
+    fill_in "Warranty", with: "2 year, manufacturer warranty"
+    fill_in "Link1", with: "http://www.amazon.com"
+    fill_in "Link1 title", with: "Amazon Link"
+    fill_in "Link2", with: "http://www.google.com"
+    fill_in "Link2 title", with: "Google Link"
+
+    click_on 'Update product'
+
+    expect(page).to have_content("Product 1")
+    expect(page).to have_content("Description of product 1")
+    expect(page).to have_content("1000")
+    expect(page).to have_content("$22.50")
+    expect(page).to have_content("50")
+    expect(page).to have_content("now")
+    expect(page).to have_content("ACME")
+    expect(page).to have_content("fkrr2qBZ")
+    expect(page).to have_content("6kqX6FbX")
+    expect(page).to have_content("$69.99")
+    expect(page).to have_content("ACME Co.")
+    expect(page).to have_content("electronics")
+    expect(page).to have_content("kitchen appliances")
+    expect(page).to have_content("702037443")
+    expect(page).to have_content("none")
+    expect(page).to have_content("02345")
+    expect(page).to have_content("2 year, manufacturer warranty")
+    expect(page).to have_content("Google Link")
+    expect(page).to have_content("Amazon Link")
   end
 end
