cf8b169fe477219674a4b3b568dcca202b582154
Irina Tagintseva <irinatag@gmail.com>
2015-03-12 18:20:55 -0700

    capybara test to update vendor

--- spec/features/vendors_spec.rb
+++ spec/features/vendors_spec.rb
@@ -55,13 +55,13 @@ feature 'CRUDing vendors' do
     expect(page).to have_content("333-333-3333")
   end
 
-  scenario 'view a vendor' do
+  scenario 'edit a vendor' do
     Vendor.create!(
-      name: 'Vendor 2',
-      description: 'Description',
+      name: 'Vendor 1',
+      description: 'Description 1',
       email_biz: 'test@test.com',
       email_finance: 'test2@test.com',
-      address: '44 Tehama St.',
+      address: '4 Tehama St.',
       city: 'SF',
       state: 'CA',
       zipcode: '00000',
@@ -70,17 +70,33 @@ feature 'CRUDing vendors' do
     )
 
     visit vendors_path
-    click_on "Vendor 2"
-    expect(page).to have_content("Vendor 2")
-    expect(page).to have_content("Description")
+
+    click_on "Vendor 1"
+    click_on "Edit"
+    fill_in "Name", with: "Vendor 1"
+    fill_in "Description", with: "Description 1"
+    fill_in "Email biz", with: "test@test.com"
+    fill_in "Email finance", with: "test2@test.com"
+    fill_in "Address", with: "44 Tehama St."
+    fill_in "City", with: "San Francisco"
+    fill_in "State", with: "CA"
+    fill_in "Zipcode", with: "94105"
+    select "United States of America", from: "Country"
+    fill_in "Phone", with: "333-333-3333"
+
+    click_on "Update Vendor"
+
+    expect(page).to have_content("Vendor 1")
+    expect(page).to have_content("Description 1")
     expect(page).to have_content("test@test.com")
     expect(page).to have_content("test2@test.com")
     expect(page).to have_content("44 Tehama St.")
-    expect(page).to have_content("SF")
+    expect(page).to have_content("San Francisco")
     expect(page).to have_content("CA")
-    expect(page).to have_content("00000")
-    expect(page).to have_content("United States of America")
+    expect(page).to have_content("94105")
+    expect(page).to have_content("US")
     expect(page).to have_content("333-333-3333")
   end
+
 
 end
