42c71fa881f002aaa95b79632b091060f191e583
Irina Tagintseva <irinatag@gmail.com>
2015-03-13 11:32:42 -0700

    add vendor_id to permitted params

--- db/schema.rb
+++ db/schema.rb
@@ -40,14 +40,11 @@ ActiveRecord::Schema.define(version: 20150313174307) do
     t.string   "link2_title"
     t.datetime "created_at"
     t.datetime "updated_at"
-<<<<<<< HEAD
+    t.integer  "vendor_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-=======
-    t.integer  "vendor_id"
->>>>>>> irina
   end
 
   create_table "vendors", force: true do |t|
