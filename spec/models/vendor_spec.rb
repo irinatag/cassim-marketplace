e22d8d853a660b6ce30d0cf7155c94562812e472
Irina Tagintseva <irinatag@gmail.com>
2015-03-13 19:27:24 -0700

    TDD validations & associations for vendors w rspec

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
-RSpec.describe Vendor, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Vendor do
+  describe 'validations' do
+    it 'requires a name' do
+      vendor = Vendor.new
+
+      vendor.valid?
+
+      expect(vendor.errors.has_key?(:name)).to eq(true)
+    end
+
+    it 'requires a business email' do
+      vendor = Vendor.new
+
+      vendor.valid?
+
+      expect(vendor.errors.has_key?(:email_biz)).to eq(true)
+    end
+
+    it 'requires a finance email' do
+      vendor = Vendor.new
+
+      vendor.valid?
+
+      expect(vendor.errors.has_key?(:email_finance)).to eq(true)
+    end
+
+    it 'requires an address' do
+      vendor = Vendor.new
+
+      vendor.valid?
+
+      expect(vendor.errors.has_key?(:address)).to eq(true)
+    end
+
+    it 'requires a city' do
+      vendor = Vendor.new
+
+      vendor.valid?
+
+      expect(vendor.errors.has_key?(:city)).to eq(true)
+    end
+
+    it 'requires a state' do
+      vendor = Vendor.new
+
+      vendor.valid?
+
+      expect(vendor.errors.has_key?(:state)).to eq(true)
+    end
+
+    it 'requires a zipcode' do
+      vendor = Vendor.new
+
+      vendor.valid?
+
+      expect(vendor.errors.has_key?(:zipcode)).to eq(true)
+    end
+
+    it 'requires a phone' do
+      vendor = Vendor.new
+
+      vendor.valid?
+
+      expect(vendor.errors.has_key?(:phone)).to eq(true)
+    end
+
+  end
+
+  describe 'associations' do
+    it 'has many products' do
+      vendor = create(:vendor)
+
+      product = vendor.products.create!(
+                name: 'name',
+                description: 'desc',
+                price: '1',
+                quantity: '1',
+                moq: 'moq')
+                
+      expect(product.vendor_id).to eq(vendor.id)
+    end
+  end
 end
