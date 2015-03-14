678152ad243298a858357f887a61c09a1e39fd80
Irina Tagintseva <irinatag@gmail.com>
2015-03-13 19:26:06 -0700

    better spacing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,20 @@
 class Product < ActiveRecord::Base
   has_attached_file :image
-  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/gif', 'image/png', 'image/jpg']
-  validates :name, :description, :quantity, :price, :vendor, :moq, presence: true
+  validates_attachment_content_type :image,
+    :content_type => [
+      'image/jpeg',
+      'image/gif',
+      'image/png',
+      'image/jpg'
+    ]
+
+  validates :name,
+    :description,
+    :quantity,
+    :price,
+    :vendor,
+    :moq,
+    presence: true
+
   belongs_to :vendor
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,13 @@
 class Vendor < ActiveRecord::Base
-  validates :name, :email_biz, :email_finance, :address, :city, :state, :zipcode, :phone, presence: true
+  validates :name,
+    :email_biz,
+    :email_finance,
+    :address,
+    :city,
+    :state,
+    :zipcode,
+    :phone,
+    presence: true
+
   has_many :products
 end
