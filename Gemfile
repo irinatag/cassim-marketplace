d443a14a11e180bffbba1a3e87b1666891a2fe39
Irina Tagintseva <irinatag@gmail.com>
2015-03-17 09:52:07 -0700

    user model w/ bcrypt encryption

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'spring',        group: :development
 gem 'factory_girl_rails'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use unicorn as the app server
 # gem 'unicorn'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316164532) do
+ActiveRecord::Schema.define(version: 20150317042136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,11 +40,18 @@ ActiveRecord::Schema.define(version: 20150316164532) do
     t.string   "link2_title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "vendor_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.integer  "vendor_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "vendors", force: true do |t|
