1e1d619682cf9fadb7753f74ddd382692ed7e45c
Irina Tagintseva <irinatag@gmail.com>
2015-03-19 13:12:56 -0700

    user sign up & sign in feature tests pass

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,17 +1,32 @@
 require 'rails_helper'
 
-feature 'users sign up' do
-  before(:each) do
-    @user = User.make(email: 'user@test.com', password: 'password')
+feature 'authorization' do
+  # before(:each) do
+  #   @user = User.make(email: 'user@test.com', password: 'password')
+  # end
+
+  scenario 'users can register' do
+    visit '/products'
+    click_on 'Sign Up'
+
+    fill_in :user_email, :with => 'user@test.com'
+    fill_in :user_password, :with => 'password'
+    fill_in :user_password_confirmation, :with => 'password'
+
+    click_button 'Sign up'
+    expect(page).to have_content 'You have successfully signed up.'
   end
 
-  scenario 'Sign in with correct credentials' do
-    visit '/sessions/new'
-    within("#session") do
-      fill_in 'Email', :with => 'user@test.com'
-      fill_in 'Password', :with => 'password'
-    end
+  scenario 'users can sign in' do
+    @user = User.create!(email: 'user@test.com', password: 'password')
+
+    visit '/products'
+    click_on 'Sign In'
+    
+    fill_in :email, :with => 'user@test.com'
+    fill_in :password, :with => 'password'
+
     click_button 'Sign in'
-    expect(page).to have_content 'Success!'
+    expect(page).to have_content 'You have successfully signed in.'
   end
 end
