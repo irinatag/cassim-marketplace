0f9909a9e2381fae70369e56e581e83b37504f7f
Irina Tagintseva <irinatag@gmail.com>
2015-03-26 09:44:51 -0700

    feature tests for registration & authentication pass

---
+++ spec/features/authentications_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+feature 'user registration' do
+  before(:each) do
+    @user = User.create!(email: 'user@test.com', password: 'password')
+  end
+
+  scenario 'users can sign in with valid email and password' do
+    # @user = User.create!(email: 'user@test.com', password: 'password')
+
+    visit '/products'
+    click_on 'Sign In'
+
+    fill_in :email, :with => 'user@test.com'
+    fill_in :password, :with => 'password'
+
+    click_button 'Sign in'
+    expect(page).to have_content 'You have successfully signed in.'
+  end
+
+  scenario 'users cannot sign in with invalid email' do
+    # @user = User.create!(email: 'user@test.com', password: 'password')
+
+    visit '/products'
+    click_on 'Sign In'
+
+    fill_in :email, :with => 'user2@test.com'
+    fill_in :password, :with => 'password'
+
+    click_button 'Sign in'
+    expect(page).to have_content 'Username / password combination is invalid'
+  end
+
+  scenario 'users cannot sign in with invalid password' do
+    # @user = User.create!(email: 'user@test.com', password: 'password')
+
+    visit '/products'
+    click_on 'Sign In'
+
+    fill_in :email, :with => 'user@test.com'
+    fill_in :password, :with => 'password1'
+
+    click_button 'Sign in'
+    expect(page).to have_content 'Username / password combination is invalid'
+  end
+
+  scenario 'users can sign out' do
+    # @user = User.create!(email: 'user@test.com', password: 'password')
+
+    visit '/products'
+    click_on 'Sign In'
+
+    fill_in :email, :with => 'user@test.com'
+    fill_in :password, :with => 'password'
+
+    click_button 'Sign in'
+    click_on 'Sign Out'
+
+    expect(page).to have_content "You have successfully signed out."
+  end
+end
---
+++ spec/features/registrations_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+feature 'user registration' do
+  # before(:each) do
+  #   @user = User.make(email: 'user@test.com', password: 'password')
+  # end
+
+  scenario 'users can register with matching password and confirmation' do
+    visit root_path
+
+    fill_in :user_email, :with => 'user@test.com'
+    fill_in :user_password, :with => 'password'
+    fill_in :user_password_confirmation, :with => 'password'
+
+    click_button 'Sign up'
+    expect(page).to have_content 'You have successfully signed up.'
+  end
+
+  scenario 'users cannot register with non-matching password and confirmation' do
+    visit root_path
+
+    fill_in :user_email, :with => 'user@test.com'
+    fill_in :user_password, :with => 'password'
+    fill_in :user_password_confirmation, :with => 'password1'
+
+    click_button 'Sign up'
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+
+  scenario 'users cannot register if email is taken' do
+    @user = User.create!(email: 'user@test.com', password: 'password')
+
+    visit root_path
+
+    fill_in :user_email, :with => 'user@test.com'
+    fill_in :user_password, :with => 'password'
+    fill_in :user_password_confirmation, :with => 'password'
+
+    click_button 'Sign up'
+    expect(page).to have_content "Email has already been taken"
+  end
+
+end
