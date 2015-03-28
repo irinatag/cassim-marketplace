2186e699af69308287f254246d2c9a228062fad2
Irina Tagintseva <irinatag@gmail.com>
2015-03-27 18:46:28 -0700

    unit tests for users pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe 'validations' do
+    it 'requires an email' do
+      user = User.new(password: 'password', password_confirmation: 'password')
+      user.save
+
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it 'requires password and password confirmation to match' do
+      user = User.new(email: 'test@test.com',
+      password: 'password',
+      password_confirmation: 'password1')
+      user.save
+
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it 'user can\'t register with same email' do
+      User.create(
+      email: 'test@test.com',
+      password: 'password',
+      password_confirmation: 'password')
+
+      user = User.new(email:'test@test.com', password: 'password', password_confirmation: 'password')
+      user.save
+
+      expect(user.errors[:email]).to include("has already been taken")
+    end
+
+  end
+
 end
