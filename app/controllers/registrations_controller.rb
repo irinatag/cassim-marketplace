95624e8135df86b3292f5502d9890ce7b0e479a5
Irina Tagintseva <irinatag@gmail.com>
2015-03-30 21:18:13 -0700

    add role to user strong params; need to figure out how to re-route to vendor new w/out STI

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,18 @@
 class RegistrationsController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.vendor?
+      redirect_to new_vendor_path, notice: "You have successfully signed up. Please register as a vendor."
+    elsif @user.save
       session[:user_id] = @user.id
       redirect_to products_path, notice: "You have successfully signed up."
     else
@@ -20,7 +26,8 @@ class RegistrationsController < ApplicationController
     params.require(:user).permit(
     :email,
     :password,
-    :password_confirmation)
+    :password_confirmation,
+    :role)
   end
 
 end
