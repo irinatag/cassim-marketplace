bbc24653c968cde852550c427800b569b9857144
Irina Tagintseva <irinatag@gmail.com>
2015-04-03 00:23:21 -0700

    cosmetic changes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,11 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate
 
   def index
+    @vendor = Vendor.all
   end
+
+  def show
+    @vendor = Vendor.find(params[:id])
+  end
+
 end
