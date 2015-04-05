a93b2aa6b258ae31118f9432ce591030921fa7b3
Irina Tagintseva <irinatag@gmail.com>
2015-04-04 23:20:40 -0700

    regex & other validations for emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
   has_secure_password
 end
