90fa92ba1e72c356ab1789cfc75ee51ef8f39398
Irina Tagintseva <irinatag@gmail.com>
2015-04-07 13:54:53 -0700

    update for order_items in cart works

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -10,7 +10,9 @@ class OrderItemsController < ApplicationController
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
+    @order_item.save
     @order_items = @order.order_items
+
   end
 
   def destroy
