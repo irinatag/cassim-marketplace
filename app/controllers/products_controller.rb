b7652453ca3e24bcf703871866698ed6f824f53e
Irina Tagintseva <irinatag@gmail.com>
2015-04-08 19:37:38 -0700

    remove cart for heroku

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,6 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.order("created_at DESC")
     @vendors = Vendor.all
-    @order_item = current_order.order_items.new
   end
 
   def show
--- app/views/order_items/update.js.erb
+++
@@ -1,2 +0,0 @@
-$(".cart-text").html("<%= escape_javascript(render 'layouts/cart_text') %>")
-$(".shopping-cart").html("<%= escape_javascript(render 'carts/shopping_cart') %>")
